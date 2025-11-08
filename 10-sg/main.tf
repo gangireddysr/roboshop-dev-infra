ad70f2b478c967477ec84ece140d811fc98280fd
gangireddysr <gangireddy.sr96@gmail.com>
2025-11-08 20:17:19 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,9 +1,24 @@
-#Catalogu
-module "catalogue" {
-  source = "terraform-aws-modules/security-group/aws"
+#Using open source module
 
-  name        = "${local.common_name_suffix}-catalogue"
-  use_name_prefix = false
-  description = "security group for catalogue with custom ports open within VPC, egress all traffic"
-  vpc_id = data.aws_ssm_parameter.vpc_id.value
+# Catalogu
+# module "catalogue" {
+#   source = "terraform-aws-modules/security-group/aws"
+
+#   name        = "${local.common_name_suffix}-catalogue"
+#   use_name_prefix = false
+#   description = "security group for catalogue with custom ports open within VPC, egress all traffic"
+#   vpc_id = data.aws_ssm_parameter.vpc_id.value
+# }
+
+
+
+module "sg" {
+  count = length(var.sg_names)
+  source = "git::https://github.com/gangireddysr/terraform-aws-sg.git?ref=main"
+  project_name = var.project_name
+  environment = var.environment
+  sg_name = var.sg_names[count.index]
+  sg_description = "Created for ${var.sg_names[count.index]}"
+  vpc_id =  local.vpc_id
 }
+
